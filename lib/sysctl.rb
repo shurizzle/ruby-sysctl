7d7fd98edd2d90b20273221ba83ddcd20b4343df
shura <shura1991@gmail.com>
2011-06-08 14:36:46 +0200

    first commit

---
+++ lib/sysctl.rb
@@ -0,0 +1,19 @@
+#--
+# Copyleft shura. [ shura1991@gmail.com ]
+#
+# This file is part of sysctl.
+#
+# sysctl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sysctl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with sysctl. If not, see <http://www.gnu.org/licenses/>.
+#++
+
